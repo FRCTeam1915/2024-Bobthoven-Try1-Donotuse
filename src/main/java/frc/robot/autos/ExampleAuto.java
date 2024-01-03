8fbdaa9ddb8f9a66174dac6d1d82e0e9f70e749c
LOSTEDs <89002511+LOSTEDs@users.noreply.github.com>
2024-01-03 06:09:48 -0500

    refactor: Adapt to the name change of `ExampleAuto`

--- src/main/java/frc/robot/autos/ExampleAuto.java
+++ src/main/java/frc/robot/autos/ExampleAuto.java
@@ -15,46 +15,49 @@ import frc.robot.Constants;
 import frc.robot.subsystems.Swerve;
 import java.util.List;
 
-public class exampleAuto extends SequentialCommandGroup {
-  public exampleAuto(Swerve s_Swerve) {
-    TrajectoryConfig config =
-        new TrajectoryConfig(
-                Constants.AutoConstants.K_MAX_SPEED_METERS_PER_SECOND,
-                Constants.AutoConstants.K_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED)
-            .setKinematics(Constants.Swerve.SWERVE_KINEMATICS);
+public class ExampleAuto extends SequentialCommandGroup {
+    public ExampleAuto(Swerve s_Swerve) {
+        TrajectoryConfig config =
+                new TrajectoryConfig(
+                        Constants.AutoConstants.K_MAX_SPEED_METERS_PER_SECOND,
+                        Constants.AutoConstants.K_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED)
+                        .setKinematics(Constants.Swerve.SWERVE_KINEMATICS);
 
-    // An example trajectory to follow.  All units in meters.
-    Trajectory exampleTrajectory =
-        TrajectoryGenerator.generateTrajectory(
-            // Start at the origin facing the +X direction
-            new Pose2d(0, 0, new Rotation2d(0)),
-            // Pass through these two interior waypoints, making an 's' curve path
-            List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
-            // End 3 meters straight ahead of where we started, facing forward
-            new Pose2d(3, 0, new Rotation2d(0)),
-            config);
+        // An example trajectory to follow.  All units in meters.
+        Trajectory exampleTrajectory =
+                TrajectoryGenerator.generateTrajectory(
+                        // Start at the origin facing the +X direction
+                        new Pose2d(0, 0, new Rotation2d(0)),
+                        // Pass through these two interior waypoints, making an 's' curve path
+                        List.of(new Translation2d(1, 1), new Translation2d(2, -1)),
+                        // End 3 meters straight ahead of where we started, facing forward
+                        new Pose2d(3, 0, new Rotation2d(0)),
+                        config
+                );
 
-    var thetaController =
-        new ProfiledPIDController(
-            Constants.AutoConstants.K_PTHETA_CONTROLLER,
-            0,
-            0,
-            Constants.AutoConstants.K_THETA_CONTROLLER_CONSTRAINTS);
-    thetaController.enableContinuousInput(-Math.PI, Math.PI);
+        var thetaController =
+                new ProfiledPIDController(
+                        Constants.AutoConstants.K_PTHETA_CONTROLLER,
+                        0,
+                        0,
+                        Constants.AutoConstants.K_THETA_CONTROLLER_CONSTRAINTS);
 
-    SwerveControllerCommand swerveControllerCommand =
-        new SwerveControllerCommand(
-            exampleTrajectory,
-            s_Swerve::getPose,
-            Constants.Swerve.SWERVE_KINEMATICS,
-            new PIDController(Constants.AutoConstants.K_PX_CONTROLLER, 0, 0),
-            new PIDController(Constants.AutoConstants.K_PY_CONTROLLER, 0, 0),
-            thetaController,
-            s_Swerve::setModuleStates,
-            s_Swerve);
+        thetaController.enableContinuousInput(-Math.PI, Math.PI);
 
-    addCommands(
-        new InstantCommand(() -> s_Swerve.resetOdometry(exampleTrajectory.getInitialPose())),
-        swerveControllerCommand);
-  }
+        SwerveControllerCommand swerveControllerCommand =
+                new SwerveControllerCommand(
+                        exampleTrajectory,
+                        s_Swerve::getPose,
+                        Constants.Swerve.SWERVE_KINEMATICS,
+                        new PIDController(Constants.AutoConstants.K_PX_CONTROLLER, 0, 0),
+                        new PIDController(Constants.AutoConstants.K_PY_CONTROLLER, 0, 0),
+                        thetaController,
+                        s_Swerve::setModuleStates,
+                        s_Swerve
+                );
+
+        addCommands(
+                new InstantCommand(() -> s_Swerve.resetOdometry(exampleTrajectory.getInitialPose())),
+                swerveControllerCommand);
+    }
 }
