4f1d5bf3133824fdd8a85cb52e069fdd24e016de
LOSTEDs <89002511+LOSTEDs@users.noreply.github.com>
2024-01-06 12:46:21 -0500

    refactor: Change `CANSparkMax.ControlType` to `CANSparkBase.ControlType`
    See -> https://codedocs.revrobotics.com/java/com/revrobotics/cansparkbase.controltype

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.sensors.CANCoder;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.ControlType;
+import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
