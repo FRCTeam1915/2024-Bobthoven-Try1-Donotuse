c0eedf396d8057714f981839d5c708f05bbbfce1
LOSTEDs <89002511+LOSTEDs@users.noreply.github.com>
2024-01-06 12:39:34 -0500

    refactor: Change `CANSparkMaxLowLevel` to `CANSparkLowLevel`
    See -> https://codedocs.revrobotics.com/java/com/revrobotics/cansparkmaxlowlevel.motortype

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.sensors.CANCoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.ControlType;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
